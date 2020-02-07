e8c1b7a7c1473317bd82eaf06712b0c2c64c4f79
Philipotieno <tmitchellb007@gmail.com>
2020-02-07 14:25:16 +0300

    write data to csv file

--- 02_WritingCsvpy.py
+++ 02_WritingCsvpy.py
@@ -4,16 +4,16 @@
 import csv
 
 # Field names
-fileds = ['Name', 'Branch', 'Year', 'CGPA']
+fileds = ['Name', 'Branch', 'Year']
 
 # Data rows of csv file
 rows = [
-    ['Philip', 'COE', '2002', '9.0'],
-    ['Mitchell', 'COE', '2002', '9.2'],
-    ['Otieno', 'IT', '2002', '9.3'],
-    ['kimberly', 'SE', '2002', '9.6'],
-    ['Wexler', 'MCE', '2002', '4.0'],
-    ['Trevor', 'EP', '2002', '9.1']
+    ['Philip', 'COE', '2002'],
+    ['Mitchell', 'COE', '2002'],
+    ['Otieno', 'IT', '2002'],
+    ['kimberly', 'SE', '2002'],
+    ['Wexler', 'MCE', '2002'],
+    ['Trevor', 'EP', '2002']
 ]
 
 # Give the csv file a name
